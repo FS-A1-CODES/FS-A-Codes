613bcb81c453b77d2cc64c624d46411c1aa506cc
Jyothi Kumar <88045362+JyothiKumar03@users.noreply.github.com>
2022-12-08 18:05:32 +0530

    Update LeastnoOfTokens.java

--- JAVA/Day-9/LeastnoOfTokens.java
+++ JAVA/Day-9/LeastnoOfTokens.java
@@ -41,3 +41,32 @@ Sample Output-2:
 -1
 
 */
+import java.util.*;
+
+public class Test{
+    public static int Tokens(int []arr,int n){
+        if(n<1)return 0;
+        int []dp=new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i:arr){
+            for(int j=i;j<=n;j++){
+                if(dp[j-i]!=Integer.MAX_VALUE)
+                dp[j]=Math.min(dp[j],dp[j-i]+1);
+            }
+        }
+        if(dp[n]!=Integer.MAX_VALUE)
+        return dp[n];
+        return -1;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String []ar=sc.nextLine().split(" ");
+        int arr[]=new int[ar.length];
+        for(int i=0;i<ar.length;i++)
+        arr[i]=Integer.parseInt(ar[i]);
+        int n=sc.nextInt();
+        System.out.println(Tokens(arr,n));
+
+    }
+}
