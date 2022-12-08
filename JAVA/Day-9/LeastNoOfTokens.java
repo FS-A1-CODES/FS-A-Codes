eeea2092f046ed64711a734bff58dea102316637
shob1313 <88094719+shob1313@users.noreply.github.com>
2022-12-08 20:50:38 +0530

    Create LeastNoOfTokens.java

---
+++ JAVA/Day-9/LeastNoOfTokens.java
@@ -0,0 +1,77 @@
+/*
+You are given some tokens printed with unique numbers on them and an integer T.
+Your task is to find the least number of tokens that you need to make up the 
+value T, by adding the numbers printed on all the tokens. 
+If you cannot make the value T, by any combination of the tokens, return -1.
+
+NOTE: Assume that you have an unlimited set of tokens of each number type.
+
+Input Format:
+-------------
+Line-1: Space separated integers tokens[], number printed on tokens.
+Line-2: An integer T.
+
+Output Format:
+--------------
+Print an integer, minimum number of tokens to make the value T.
+
+
+Sample Input-1:
+---------------
+1 2 5
+11
+
+Sample Output-1:
+----------------
+3
+
+Explanation:
+------------
+5+5+1 = 11
+
+
+Sample Input-2:
+---------------
+2 4
+15
+
+Sample Output-2:
+----------------
+-1
+
+*/
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String arr[] = sc.nextLine().split(" ");
+        int n = arr.length;
+        int token[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            token[i] = Integer.parseInt(arr[i]);
+        }
+        int sum = sc.nextInt();
+        System.out.println(noTokens(token,sum,n));
+    }
+    public static int noTokens(int[] token,int sum,int n)
+    {
+        int dp[] = new int[sum+1];
+        int dplen = sum+1;
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j = token[i];j<dplen;j++)
+            {
+                if(dp[j-token[i]]!=Integer.MAX_VALUE)
+                {
+                    dp[j]=Math.min(dp[j],dp[j-token[i]]+1);
+                }
+            }
+        }
+        return dp[dplen-1]==Integer.MAX_VALUE?-1:dp[dplen-1];
+    }
+}
